1ed1261f0cb345f546592c24724f58d8f7751037
Chris Wong <chris@cogifire.com>
2015-12-06 16:47:20 -0800

    v1.0

---
+++ README.md
@@ -0,0 +1,159 @@
+# node-all-paths
+
+> JavaScript implementation to find all possible paths in a graph
+
+## Installation
+
+This plugin uses some ES6 features. You can run the latest version on **NodeJS `v4.0.0` or newer**
+
+```shell
+npm install https://github.com/chriswongtv/node-all-paths.git --save
+```
+
+## Usage
+
+Basic example:
+
+```js
+const Graph = require('node-all-paths')
+
+const graph = new Graph()
+
+graph.addNode('A', { B:1 });
+graph.addNode('B', { C:2, D: 4 });
+graph.addNode('C', { D:1 });
+graph.addNode('D', { });
+
+route.path('A', 'D') // => [ [ "A", "B", "C", "D" ], [ "A", "B", "D" ] ]
+```
+
+## API
+
+### `Graph([nodes])`
+
+#### Parameters
+
+- `Object nodes` _optional_: Initial nodes graph.
+
+A nodes graph must follow this structure:
+
+```
+{
+  node: {
+    neighbor: cost Number
+  }
+}
+```
+
+```js
+{
+  'A': {
+    'B': 1
+  },
+  'B': {
+    'C': 2,
+    'D': 4
+  }
+}
+```
+
+#### Example
+
+```js
+const route = new Graph()
+
+// or with pre-populated graph
+const route = new Graph({
+  'A': { 'B': 1 },
+  'B': { 'A': 1, 'C': 2, 'D': 4 }
+})
+```
+
+
+
+### `Graph#addNode(name, edges)`
+
+Add a node to the nodes graph
+
+#### Parameters
+
+- `String name`: name of the node
+- `Object edges`: object containing the name of the neighboring nodes and their cost
+
+#### Returns
+
+Returns `this` allowing chained calls.
+
+```js
+const route = new Graph()
+
+route.addNode('A', { B: 1 })
+
+// chaining is possible
+route.addNode('B', { C: 3 }).addNode('C', { });
+```
+
+### `Graph#path(start, goal [, options])`
+
+#### Parameters
+
+- `String start`: Name of the starting node
+- `String goal`: Name of out goal node
+- `Object options` _optional_: Addittional options:
+  - `Boolean trim`, deafult `false`: If set to true, the result won't include the start and goal nodes
+  - `Boolean reverse`, default `false`: If set to true, the result will be in reverse order, from goal to start
+  - `Boolean cost`, default `false`: If set to true, an object will be returned with the following keys:
+    - `Array path`: Computed path (subject to other options)
+    - `Number cost`: Total cost for the found path
+
+#### Returns
+
+If `options.cost` is `false` (default behaviour) an `Array` will be returned, containing the name of the crossed nodes. By default it will be ordered from start to goal, and those nodes will also be included. This behaviour can be changes with `options.trim` and `options.reverse` (see above)
+
+If `options.cost` is `true`, an `Object` with keys `path` and `cost` will be returned. `path` follows the same rules as above and `cost` is the total cost of the found route between nodes.
+
+When to route can be found, the path will be set to `null`.
+
+```js
+const Graph = require('node-all-paths')
+
+const route = new Graph()
+
+route.addNode('A', { B:1 });
+route.addNode('B', { C:2, D: 4 });
+route.addNode('C', { D:1 });
+route.addNode('D', { });
+
+route.path('A', 'D') // => [ [ "A", "B", "C", "D" ], [ "A", "B", "D" ] ]
+
+// trimmed
+route.path('A', 'D', { trim: true }) // => [ [ "B", "C" ], [ "B" ] ]
+
+// reversed
+route.path('A', 'D', { reverse: true }) // => [ [ "D", "C", "B", "A" ], [ "D", "B", "A" ] ]
+
+// include the cost
+route.path('A', 'D', { cost: true })
+// => [
+//      {
+//        "path": [
+//          "A",
+//          "B",
+//          "C",
+//          "D"
+//        ],
+//        "cost": 1
+//      },
+//      {
+//        "path": [
+//          "A",
+//          "B",
+//          "D"
+//        ],
+//        "cost": 4
+//      }
+//    ]
+```
+
+[1]: https://github.com/andrewhayward/dijkstra
+[2]: https://github.com/albertorestifo/node-dijkstra
---
+++ libs/Graph.js
@@ -0,0 +1,135 @@
+'use strict'
+
+const Queue = require('./PriorityQueue')
+const populateMap = require('./populateMap')
+
+class Graph {
+
+  /**
+   * Constrict the graph
+   *
+   * @param {object} [graph] - Nodes to initiate the graph with
+   */
+  constructor (graph) {
+    this.graph = new Map()
+
+    if (graph) populateMap(this.graph, graph, Object.keys(graph))
+  }
+
+  /**
+   * Add a node to the graph
+   *
+   * @param {string} name      - Name of the node
+   * @param {object} neighbors - Neighbouring nodes and cost to reach them
+   */
+  addNode (name, neighbors) {
+    let _neighbors = new Map()
+
+    populateMap(_neighbors, neighbors, Object.keys(neighbors))
+    this.graph.set(name, _neighbors)
+
+    return this
+  }
+
+  /**
+   * Compute the shortest path between the specified nodes
+   *
+   * @param {string}  start     - Starting node
+   * @param {string}  goal      - Node we want to reach
+   * @param {object}  [options] - Options
+   *
+   * @param {boolean} [options.trim]    - Exclude the origin and destination nodes from the result
+   * @param {boolean} [options.reverse] - Return the path in reversed order
+   * @param {boolean} [options.cost]    - Also return the cost of the path when set to true
+   *
+   * @return {array|object} Computed path between the nodes.
+   *  When `option.cost` is set to true, the returned value will be an object
+   *  with keys:
+   *
+   *    - `Array path`: Computed path between the nodes
+   *    - `Number cost`: Cost of the path
+   */
+  path (start, goal, options) {
+    options = options || {}
+
+    // Don't run when we don't have nodes set
+    if (!this.graph.size) {
+      if (options.cost) return { path: null, cost: 0 }
+
+      return null
+    }
+
+    let explored = new Set()
+    let frontier = new Queue()
+    let previous = new Map()
+
+    let paths = []
+    let totalCost = 0
+
+    // Add the starting point to the frontier, it will be the first node visited
+    frontier.set(start, 0)
+
+    recursive(this.graph, frontier.next());
+
+    function recursive(graph, node) {
+      // When the node with the lowest cost in the frontier in our goal node,
+      // we can compute the path and exit the loop
+      if (node.key === goal) {
+        let path = [];
+        // Set the total cost to the current value
+        totalCost = node.priority
+
+        let _nodeKey = node.key
+        while (previous.has(_nodeKey)) {
+          path.push(_nodeKey)
+          _nodeKey = previous.get(_nodeKey)
+        }
+
+        // Remove the first value (the goal node) if we want a trimmed result
+        if (options.trim) {
+          path.shift()
+        } else {
+          // Add the origin waypoint at the end of the array
+          path = path.concat([ start ])
+        }
+
+        // Reverse the path if we don't want it reversed, so the result will be
+        // from `start` to `goal`
+        if (!options.reverse) {
+          path = path.reverse()
+        }
+
+        // Return an object if we also want the cost
+        if (options.cost) {
+          paths.push({
+            path: path,
+            cost: totalCost
+          })
+        } else
+          paths.push(path)
+
+        return;
+      } else {
+        // Loop all the neighboring nodes
+        let neighbors = graph.get(node.key) || new Map()
+        neighbors.forEach(function (_cost, _node) {
+          var element = { 'key': _node, 'priority': _cost };
+          previous.set(_node, node.key);
+          recursive(graph, element);
+        })
+      }
+    }
+
+    // Return null when no path can be found
+    if (!paths.length) {
+      if (options.cost) return { paths: null, cost: 0 }
+
+      return null
+    }
+
+    return paths
+  }
+
+}
+
+module.exports = Graph
